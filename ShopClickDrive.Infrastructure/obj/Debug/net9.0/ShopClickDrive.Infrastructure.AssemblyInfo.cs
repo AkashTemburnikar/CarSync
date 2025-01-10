84731f47437fc9e577a721311e2a41a9ade7fd94
Akash <59678417+AkashTemburnikar@users.noreply.github.com>
2025-01-10 15:23:39 -0600

    Commit

--- ShopClickDrive.Infrastructure/obj/Debug/net9.0/ShopClickDrive.Infrastructure.AssemblyInfo.cs
+++ ShopClickDrive.Infrastructure/obj/Debug/net9.0/ShopClickDrive.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopClickDrive.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb07ca1f412646a9e93747c95f1fa91c2b5dcf23")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopClickDrive.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopClickDrive.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
