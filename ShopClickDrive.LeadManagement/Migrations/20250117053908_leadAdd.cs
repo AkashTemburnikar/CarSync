247a5c1e0856955794a2d5384c4b924848a3d602
Akash <59678417+AkashTemburnikar@users.noreply.github.com>
2025-01-16 23:42:20 -0600

    EF Migrations Lead Management

---
+++ ShopClickDrive.LeadManagement/Migrations/20250117053908_leadAdd.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopClickDrive.LeadManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class leadAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
